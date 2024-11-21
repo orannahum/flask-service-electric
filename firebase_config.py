2884f15f4e165bdea17eb6c110b7a5659ddd4141
orannahum <oranne5@gmail.com>
2024-11-21 12:57:49 +0200

    Add Firebase Hosting GitHub Actions workflow

---
+++ firebase_config.py
@@ -0,0 +1,9 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyBQqie-YePLrpQQBVymuSSTVLGo9FaTOKo",
+  "authDomain": "electric-price-optimize.firebaseapp.com",
+  "projectId": "electric-price-optimize",
+  "storageBucket": "electric-price-optimize.firebasestorage.app",
+  "messagingSenderId": "630255254586",
+  "appId": "1:630255254586:web:f3510018a4a5bea6d0c903",
+  "measurementId": "G-TM171H3SR7"
+}
